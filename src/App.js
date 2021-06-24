b06b5e3bafe78f1a81f1714517da5fadb9b7caea
DJMF2015 <djm.fulton@gmail.com>
2021-06-24 17:51:16 +0100

    basic functionalities completed to filter and remove a task and toggle between state. Data hardcoded for now.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+//stateless componnet 
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+    //fake api
+    {
+      id: 1,
+      text: 'Doctors appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Go to the museum',
+      day: 'Dec 5th at 22:30pm',
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: 'Attend Club Day',
+      day: 'June 24 at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: 'Learn react',
+      day: 'Feb 15th at 15:30pm',
+      reminder: true,
+    },
+
+  ])
+  //DELETE TASK
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+const toggleReminder = (id)=> {
+ setTasks(
+  tasks.map((task) => 
+ task.id === id ? 
+ {...task, reminder: !task.reminder} : task)
+ )
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete=
+          {deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No Tasks To Show'
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
